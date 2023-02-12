a0e7d2f0034329ac0e4efd038dd1d7d3e900f0c0
RxD <solidfire444@hotmail.com>
2023-02-12 14:45:40 -0800

    1.33.1
    Building Warnings 1.33.1 Disable building sound option.

--- BuildingWarnings.h
+++ BuildingWarnings.h
@@ -1,3 +1,12 @@
+/*
+	Building Warnings
+	Copyright 2020 Unstoppable
+
+	This plugin is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version. See GNU General Public License for more details.
+*/
 
 #ifndef BuildingWarnings
 #define BuildingWarnings
@@ -5,29 +14,76 @@
 #include "general.h"
 #include "da_gamefeature.h"
 #include "da_event.h"
-#include "da_player.h"
+
+#define CompileTime StringFormat("%s %s", __DATE__, __TIME__)
+#define Version 1.331f
+
+enum WarnState
+{
+	WARNSTATE_REPAIRING = 0,
+	WARNSTATE_75,
+	WARNSTATE_50,
+	WARNSTATE_25
+};
+
+struct FormatParams
+{
+	WarnState State;
+	StringClass Message;
+	int Team;
+	GameObject* Building;
+	GameObject* Damager;
+	StringClass *Out;
+};
 
 class BuildingWarningsPlayerDataClass : public DAPlayerDataClass {
 public:
 	virtual void Init() {
-		WarningsSounds = true;
+		WarningSounds = true;
 	}
-	bool WarningsSounds;
+	bool WarningSounds;
 };
 
-
-class BuildingWarningsGameFeatureClass : public DAEventClass, public DAGameFeatureClass, public DAPlayerDataManagerClass<BuildingWarningsPlayerDataClass>   {
+class BuildingWarningsGameFeatureClass : public DAEventClass, public DAGameFeatureClass, public DAPlayerDataManagerClass<BuildingWarningsPlayerDataClass> {
 public:
-	virtual void Init();
-	virtual ~BuildingWarningsGameFeatureClass();
-	virtual void Damage_Event(DamageableGameObj *Victim, ArmedGameObj *Damager, float Damage, unsigned int Warhead, float Scale, DADamageType::Type Type);
-	virtual void Timer_Expired(int Number, unsigned int Data);
-	virtual bool Chat_Command_Event(cPlayer *Player,TextMessageEnum Type,const StringClass &Command,const DATokenClass &Text,int ReceiverID);
+	void SendStateMessage(FormatParams *Params);
+	void FormatStateMessage(FormatParams *Params);
+protected:
+	void Init() override;
+	void Settings_Loaded_Event() override;
+	void Damage_Event(DamageableGameObj* Victim, ArmedGameObj* Damager, float Damage, unsigned int Warhead, float Scale, DADamageType::Type Type) override;
+	void Timer_Expired(int Number, unsigned int Data) override;
+	bool Chat_Command_Event(cPlayer *Player,TextMessageEnum Type,const StringClass &Command,const DATokenClass &Text,int ReceiverID);
 	void Create_2D_Sound_Enabled_Team(int Team, StringClass Sound);
+	~BuildingWarningsGameFeatureClass();
+private:
+	static const char Sounds[4][2][50];
+	static DASettingsClass *INI;
 
-	bool Announce75;
-	bool Announce50;
-	bool Announce25;
+	/* static */ StringClass Format75;
+	/* static */ StringClass Format50;
+	/* static */ StringClass Format25;
+	/* static */ StringClass FormatRepairing;
+	/* static */ DynamicVectorClass<StringClass> IncludeList;
+	/* static */ DynamicVectorClass<StringClass> ExcludeList;
+	/* static */ bool ReportVehicleBuildings;
+	/* static */ bool ShowEnemy;
+	/* static */ bool UseDAPrefix;
+	/* static */ bool UseTeamColors;
+	/* static */ float PercentCooldown;
+	/* static */ float RepairCooldown;
+	/* static */ Vector3 Color75;
+	/* static */ Vector3 Color50;
+	/* static */ Vector3 Color25;
+	/* static */ Vector3 ColorRepairing;
+	/* static */ bool Enable75;
+	/* static */ bool Enable50;
+	/* static */ bool Enable25;
+	/* static */ bool EnableRepairing;
+
+	//bool Announce75;
+	//bool Announce50;
+	//bool Announce25;
 };
 
 #endif
